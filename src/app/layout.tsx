5b0566d9b08ffe3e46d7c5106a7c0d124efb292b
Mario <mariomad2296@gmail.com>
2025-01-22 19:49:43 +0800

    Update font from `Geist` to `Poppins`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+
+import { Poppins } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
+
 import './globals.css';
 
-const geistSans = localFont({
-    src: './fonts/GeistVF.woff',
-    variable: '--font-geist-sans',
-    weight: '100 900',
-});
-const geistMono = localFont({
-    src: './fonts/GeistMonoVF.woff',
-    variable: '--font-geist-mono',
-    weight: '100 900',
+const poppins = Poppins({
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-poppins',
 });
 
 export const metadata: Metadata = {
@@ -24,8 +24,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+        <html lang="en" className={cn(poppins.variable)}>
+            <body className="antialiased">{children}</body>
         </html>
     );
 }
