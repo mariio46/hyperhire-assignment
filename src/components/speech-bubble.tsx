34a91c2cade0e493b4c5ab3f07221e9fcf566a0e
Mario <mariomad2296@gmail.com>
2025-01-23 18:03:14 +0800

    Update speech-bubble.tsx

--- src/components/speech-bubble.tsx
+++ src/components/speech-bubble.tsx
@@ -1,31 +1,39 @@
+import * as React from 'react';
+
+import { cva, type VariantProps } from 'class-variance-authority';
+
 import { cn } from '@/lib/utils';
 
-const SpeechBubbleLeftArrow = ({ children, className, ...props }: React.ComponentProps<'div'>) => {
-    return (
-        <div
-            className={cn(
-                'relative inline-block rounded-lg bg-[#8BC4FF] px-4 py-2 text-lg font-black text-white',
-                className,
-            )}
-            {...props}>
-            {children}
-            <div className="absolute -bottom-3 left-4 size-3 -translate-y-2 rotate-45 bg-[#8BC4FF]"></div>
-        </div>
-    );
-};
+const speechBubbleVariants = cva('relative inline-flex rounded-lg px-4 py-2 text-lg font-black', {
+    variants: {
+        variant: {
+            default: 'bg-[#8BC4FF] [&>[data-slot=arrow]]:bg-[#8BC4FF] text-white',
+            primary: 'bg-[#E9F7EF] [&>[data-slot=arrow]]:bg-[#E9F7EF] text-[#00C696]',
+        },
+        arrowPosition: {
+            left: '[&>[data-slot=arrow]]:absolute [&>[data-slot=arrow]]:-bottom-3 [&>[data-slot=arrow]]:left-4 [&>[data-slot=arrow]]:size-3 [&>[data-slot=arrow]]:-translate-y-2 [&>[data-slot=arrow]]:rotate-45',
+            middle: '[&>[data-slot=arrow]]:absolute [&>[data-slot=arrow]]:-bottom-3 [&>[data-slot=arrow]]:left-1/2 [&>[data-slot=arrow]]:size-3 [&>[data-slot=arrow]]:-translate-x-1/2 [&>[data-slot=arrow]]:-translate-y-2 [&>[data-slot=arrow]]:rotate-45',
+        },
+    },
+    defaultVariants: {
+        variant: 'default',
+        arrowPosition: 'left',
+    },
+});
 
-const SpeechBubbleMiddleArrow = ({ children, className, ...props }: React.ComponentProps<'div'>) => {
-    return (
-        <div
-            className={cn(
-                'relative inline-flex items-center rounded-lg bg-[#E9F7EF] px-4 py-2 text-lg font-black text-[#00C696]',
-                className,
-            )}
-            {...props}>
-            {children}
-            <div className="absolute -bottom-3 left-1/2 size-3 -translate-x-1/2 -translate-y-2 rotate-45 bg-[#E9F7EF]"></div>
-        </div>
-    );
-};
+interface SpeechBubbleProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof speechBubbleVariants> {
+    //
+}
 
-export { SpeechBubbleLeftArrow, SpeechBubbleMiddleArrow };
+const SpeechBubble = React.forwardRef<HTMLDivElement, SpeechBubbleProps>(
+    ({ className, variant, arrowPosition, children, ...props }, ref) => {
+        return (
+            <div className={cn(speechBubbleVariants({ variant, className, arrowPosition }))} ref={ref} {...props}>
+                {children}
+                <div data-slot="arrow" />
+            </div>
+        );
+    },
+);
+
+export { SpeechBubble };
